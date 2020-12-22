24143e5c1f654a5581a479ea5717fdb89311e70b
developerxyzw <44705138+developerxyzw@users.noreply.github.com>
2020-12-22 15:23:22 +0300

    Update gmath.cpp
    Add support for task 16

--- gmath.cpp
+++ gmath.cpp
@@ -15,6 +15,13 @@ gMathConst *gMathConst::getInstance()
 	return &gmc;
 }
 
+long double gabs(long double val)
+{
+    if (val > -the_eps)
+        return val;
+    return -val;
+}
+
 
 //=========================================
 pt::pt(long double x /*= 0*/, long double y /*= 0*/)
@@ -42,7 +49,7 @@ long double pt::len2() const
 void pt::norm()
 {
 	double dOldLen = len();
-	if (abs(dOldLen) < eps)
+	if (gabs(dOldLen) < the_eps)
 	{
 		return;
 	}
@@ -69,17 +76,17 @@ long double pt::dp(const pt &w) const
 
 long double pt::angle(const pt &w) const
 {
-	return abs(atan2(cp(w), dp(w)));
+	return gabs(atan2(cp(w), dp(w)));
 }
 
 long double pt::angle2(const pt &w) const
 {
 	long double a = relangle(w);
-	if (a < eps)
+	if (a < the_eps)
 	{
 		a += 2 * the_PI;
 	}
-	return abs(a);
+	return gabs(a);
 }
 
 long double pt::relangle(const pt &w) const
@@ -158,8 +165,8 @@ bool seg::cont(const pt &p) const
 {
 	long double cp1 = pt(A, B).cp(pt(A, p));
 	long double dp1 = pt(p, A).dp(pt(p, B));
-	bool XinLine = (abs(cp1) < eps);
-	bool XbtwAB = (dp1 < eps);
+	bool XinLine = (gabs(cp1) < the_eps);
+	bool XbtwAB = (dp1 < the_eps);
 	return XinLine && XbtwAB;
 }
 
@@ -196,7 +203,7 @@ line::line(const pt &a, const line &b)
 	long double OH = dist(a, b);
 	long double PH = sqrt(pt(a, P).len2() - OH * OH);
 	pt H = P + b.norm().ortho().getNorm() * PH;
-	if (abs(dist(a, H) - OH) > eps)
+	if (gabs(dist(a, H) - OH) > the_eps)
 	{
 		H = P + -b.norm().ortho().getNorm() * PH;
 	}
@@ -205,14 +212,14 @@ line::line(const pt &a, const line &b)
 
 bool line::operator^(const line &b) const
 {
-	return abs(pt(A, B).cp(pt(b.A, b.B))) > eps;
+	return gabs(pt(A, B).cp(pt(b.A, b.B))) > the_eps;
 }
 
 bool line::operator==(const line &b) const
 {
-	return abs(A * b.B - b.A * B) < eps
-		&& abs(A * b.C - b.A * C) < eps
-		&& abs(B * b.C - b.B * C) < eps;
+	return gabs(A * b.B - b.A * B) < the_eps
+		&& gabs(A * b.C - b.A * C) < the_eps
+		&& gabs(B * b.C - b.B * C) < the_eps;
 }
 
 pair<int, pt> line::operator*(const line &L2) const
@@ -249,13 +256,45 @@ long double line::operator()(const pt &a) const
 
 bool line::cont(const pt &a) const
 {
-	return (*this)(a) < eps;
+	return gabs((*this)(a)) < the_eps;
 }
 
 pt line::getPoint() const
 {
 	double aaa = -C / (A * A + B * B);
 	return pt(aaa * A, aaa * B);
+}
+
+
+//=========================================
+ray::ray(const pt &A /*= pt(0, 0)*/, const pt &B /*= pt(1, 0)*/)
+	: A(A)
+	, B(B)
+{
+}
+
+bool ray::operator^(const ray &b) const
+{
+	pair<int, pt> inter = ln() * b.ln();
+	if (inter.first == 0) return false;
+	if (inter.first == 1) return cont(inter.second) && b.cont(inter.second);
+	if (areCodirect(b)) return true;
+	return cont(b.A);
+}
+
+bool ray::cont(const pt &p) const
+{
+	return (A + -p).len2() < the_eps || (ln().cont(p) && (gabs((p + -A).cp(B + -A)) < the_eps) && ((p + -A).dp(B + -A) > -the_eps));
+}
+
+line ray::ln() const
+{
+	return line(A, B);
+}
+
+bool ray::areCodirect(const ray &b) const
+{
+	return !(ln() ^ b.ln()) && (gabs((B + -A).cp(b.B + -b.A)) < the_eps) && ((B + -A).dp(b.B + -b.A) > -the_eps);
 }
 
 
@@ -274,7 +313,7 @@ long double poly::area() const
 	}
 	res += pts.front().cp(pts.back());
 	res /= 2;
-	return abs(res);
+	return gabs(res);
 }
 
 long double poly::per() const
@@ -301,7 +340,7 @@ bool poly::cont(const pt &a) const
 	}
 	for (int i = 0; !ok && i < n; ++i)
 	{
-		ok = abs(dist(v[i], a)) < eps;
+		ok = gabs(dist(v[i], a)) < the_eps;
 	}
 	if (!ok)
 	{
@@ -312,7 +351,7 @@ bool poly::cont(const pt &a) const
 			int j = (i + 1) % n;
 			an += pt(a, v[i]).relangle(pt(a, v[j]));
 		}
-		if ((int)round(abs(an / (2 * the_PI))) == 1)
+		if ((int)round(gabs(an / (2 * the_PI))) == 1)
 		{
 			ok = true;
 		}
@@ -324,21 +363,21 @@ bool poly::cont(const pt &a) const
 	return ok;
 }
 
-bool poly::cont_log(const pt & a) const
+bool poly::cont_log(const pt &a) const
 {
-	if (dist(a, zero) < eps)
+	if (dist(a, zero) < the_eps)
 	{
 		return true;
 	}
 
 	// bsearch
 	long double an = -pt(zero, a).relangle(pt(0, -1));
-	if (an < eps)
+	if (an < the_eps)
 	{
 		return false;
 	}
 	auto it = upper_bound(polar.begin(), polar.end(), an);
-	if (it == polar.end() && abs(an - polar.back()) < eps)
+	if (it == polar.end() && gabs(an - polar.back()) < the_eps)
 	{
 		it = polar.end() - 1;
 	}
@@ -366,7 +405,7 @@ void poly::cont_log_init()
 	zero = pts[0];
 	for (int i = 1; i < pts.size(); ++i)
 	{
-		if (pts[i].x < zero.x || (abs(pts[i].x - zero.x) < eps && pts[i].y < zero.y))
+		if (pts[i].x < zero.x || (gabs(pts[i].x - zero.x) < the_eps && pts[i].y < zero.y))
 		{
 			zero = pts[i];
 			i_zero = i;
@@ -394,7 +433,7 @@ bool poly::isConvex()
 	bool ok = true;
 	for (int i = 1; ok && i <= n; ++i)
 	{
-		if (pt(p[i], p[i - 1]).angle2(pt(p[i], p[i + 1])) > the_PI + eps)
+		if (pt(p[i], p[i - 1]).angle2(pt(p[i], p[i + 1])) > the_PI + the_eps)
 		{
 			ok = false;
 		}
@@ -413,7 +452,7 @@ void poly::reverseCW()
 		int k = (i + 2) % n;
 		an += pt(p[j], p[i]).angle2(pt(p[j], p[k]));
 	}
-	bool rev = abs(an - the_PI * (n - 2)) < eps;
+	bool rev = gabs(an - the_PI * (n - 2)) < the_eps;
 	if (!rev)
 	{
 		reverse(p.begin() + 1, p.end());
@@ -431,7 +470,7 @@ void poly::reverseCCW()
 		int k = (i + 2) % n;
 		an += pt(p[j], p[i]).angle2(pt(p[j], p[k]));
 	}
-	bool rev = abs(an - the_PI * (n - 2)) < eps;
+	bool rev = gabs(an - the_PI * (n - 2)) < the_eps;
 	if (rev)
 	{
 		reverse(p.begin() + 1, p.end());
@@ -449,16 +488,16 @@ krug::krug(const pt &O /*= pt(0, 0)*/, long double R /*= 0*/)
 vector<pt> krug::operator*(const krug &b) const
 {
 	long double d = pt(center, b.center).len2();
-	if (abs(d) < eps && abs(rad - b.rad) < eps)
+	if (gabs(d) < the_eps && gabs(rad - b.rad) < the_eps)
 	{
 		// INF int
 		return vector<pt>(3);
 	}
-	else if (abs(d - (rad - b.rad)*(rad - b.rad)) < eps || abs(d - (rad + b.rad)*(rad + b.rad)) < eps)
+	else if (gabs(d - (rad - b.rad) * (rad - b.rad)) < the_eps || gabs(d - (rad + b.rad) * (rad + b.rad)) < the_eps)
 	{
 		// 1 kas
 		pt O1O2(center, b.center);
-		if (abs(d - (rad - b.rad)*(rad - b.rad)) < eps && rad < b.rad)
+		if (gabs(d - (rad - b.rad) * (rad - b.rad)) < the_eps && rad < b.rad)
 		{
 			// INNER case
 			O1O2 = -O1O2;
@@ -467,7 +506,7 @@ vector<pt> krug::operator*(const krug &b) const
 		pt I = center + (norm_O1O2 * rad);
 		return{ I };
 	}
-	else if (d < (rad - b.rad)*(rad - b.rad) || d >(rad + b.rad)*(rad + b.rad))
+	else if (d < (rad - b.rad) * (rad - b.rad) || d > (rad + b.rad) * (rad + b.rad))
 	{
 		// 0 int
 		return vector<pt>(0);
@@ -501,9 +540,9 @@ triangle::type triangle::getType() const
 	long double a1 = pt(A, B).angle(pt(A, C));
 	long double a2 = pt(B, A).angle(pt(B, C));
 	long double a3 = the_PI - (a1 + a2);
-	if (abs(a1 - the_PI2) < eps
-		|| abs(a2 - the_PI2) < eps
-		|| abs(a3 - the_PI2) < eps)
+	if (gabs(a1 - the_PI2) < the_eps
+		|| gabs(a2 - the_PI2) < the_eps
+		|| gabs(a3 - the_PI2) < the_eps)
 	{
 		return type::right;
 	}
@@ -552,7 +591,7 @@ triangle triangle::fromLengths(long double a, long double b, long double c) cons
 	// cos(ab) = (a^2 - c^2 + b^2) / (2ab)
 	long double co = (a * a + b * b - c * c) / (2 * a * b);
 	t.C = t.B.getNorm().rot(acos(co)) * a;
-	
+
 	return t;
 }
 
@@ -560,7 +599,7 @@ triangle triangle::fromLengths(long double a, long double b, long double c) cons
 //=========================================
 long double dist(const line &a, const pt &b)
 {
-	return abs(a(b) / (a.norm().len()));
+	return gabs(a(b) / (a.norm().len()));
 }
 
 long double dist(const pt &a, const line &b)
@@ -596,9 +635,86 @@ long double dist(const seg &a, const pt &b)
 	return dist(b, a);
 }
 
+long double dist(const line &a, const line &b)
+{
+	if (a ^ b) return 0;
+	return dist(a.getPoint(), b);
+}
+
+long double dist(const line &a, const seg &b)
+{
+	pair<int, pt> inter = a * line(b.A, b.B);
+	if (inter.first == 1 && b.cont(inter.second)) return 0;
+	return min(dist(b.A, a), dist(b.B, a));
+}
+
+long double dist(const seg &a, const line &b)
+{
+	return dist(b, a);
+}
+
+long double dist(const seg &a, const seg &b)
+{
+	if (a ^ b) return 0;
+	return min(min(dist(a.A, b), dist(a.B, b)), min(dist(b.A, a), dist(b.B, a)));
+}
+
+long double dist(const pt &a, const ray &b)
+{
+	long double a1 = pt(b.A, a).angle(pt(b.A, b.B));
+	if (a1 > the_PI2)
+	{
+		return dist(a, b.A);
+	}
+	else if (b.cont(a))
+	{
+		return 0;
+	}
+	else
+	{
+		return dist(a, line(b.A, b.B));
+	}
+}
+
+long double dist(const ray &a, const pt &b)
+{
+	return dist(b, a);
+}
+
+long double dist(const line &a, const ray &b)
+{
+	pair<int, pt> inter = a * b.ln();
+	if (inter.first == 0) return dist(a, b.ln());
+	if (inter.first == 2) return 0;
+	if (b.cont(inter.second)) return 0;
+	return dist(b.A, a);
+}
+
+long double dist(const ray &a, const line &b)
+{
+	return dist(b, a);
+}
+
+long double dist(const ray &a, const ray &b)
+{
+	if (a ^ b) return 0;
+	return min(dist(a, b.A), dist(a.A, b));
+}
+
+long double dist(const ray &a, const seg &b)
+{
+	if ((ray(b.A, b.B) ^ a) && (ray(b.B, b.A) ^ a)) return 0;
+	return min(dist(a.A, b), min(dist(b.A, a), dist(b.B, a)));
+}
+
+long double dist(const seg &a, const ray &b)
+{
+	return dist(b, a);
+}
+
 
 //=========================================
-istream &operator >> (istream &is, pt &p)
+istream &operator>>(istream &is, pt &p)
 {
 	is >> p.x >> p.y;
 	return is;
@@ -610,7 +726,7 @@ ostream &operator<<(ostream &os, const pt &p)
 	return os;
 }
 
-istream &operator >> (istream &is, seg &p)
+istream &operator>>(istream &is, seg &p)
 {
 	is >> p.A >> p.B;
 	return is;
@@ -622,7 +738,7 @@ ostream &operator<<(ostream &os, const seg &p)
 	return os;
 }
 
-istream &operator >> (istream &is, krug &p)
+istream &operator>>(istream &is, krug &p)
 {
 	is >> p.center >> p.rad;
 	return is;
